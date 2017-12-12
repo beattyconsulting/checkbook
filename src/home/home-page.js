258d8b86c8f2a3269f41a340560cbae92f6820f3
beattyk1 <beattyphone@gmail.com>
2017-12-11 19:42:52 -0600

    Add react-media/viewport to detect mobile.

--- src/home/home-page.js
+++ src/home/home-page.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 import {Transaction} from '../components/transaction';
+import Media from 'react-media'
 
 const base = 'home-page'
 
 export class HomePage extends Component {
-
-  render () {
+  render() {
     return (
-      <div className={`${base}`}>
-        <Transaction />
+      <div>
+        <Media query="(max-width: 599px)">
+          {matches => matches ? (
+            <div className='checkbook-small'>
+                <div className={`${base}`}><Transaction /></div>
+            </div>
+          ) : (
+            <div><div className={`${base}`}><Transaction /></div></div>
+          )}
+        </Media>
       </div>
     )
   }
